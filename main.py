7e33cf582e46bc246fa008d7ee71587a48665875
kushalg36 <kushalgupta_de@srmuniv.edu.in>
2020-03-26 17:00:37 +0530

    loggers added

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from bs4 import BeautifulSoup as bs4
 import requests
 from tabulate import tabulate
 import json
+import logging
 
+logging.basicConfig(format='%(asctime)s %(message)s',filename='program_logs.log',level=logging.DEBUG,filemode='a')
 def func(row):
     return [x.get_text().replace('\n','') for x in row]
 
@@ -17,48 +19,60 @@ def load():
 
 header = ['Name of State / UT','Confirmed cases(Indian National)','Confirmed cases(Foreign National)','Cured','Death']
 
-response = requests.get('https://www.mohfw.gov.in/',timeout=5).text
 
-soup = bs4(response,'lxml')
 
-table_content = soup.find_all('table', class_='table table-striped table-dark')
+if __name__ == '__main__':
+    try:
+        response = requests.get('https://www.mohfw.gov.in/',timeout=5).text
 
-mentioned_table = table_content[-1]
-mentioned_rows = mentioned_table.find_all('tr')
-mentioned_rows = mentioned_rows[1:-1]
+        soup = bs4(response,'lxml')
 
-data = []
+        table_content = soup.find_all('table', class_='table table-striped table-dark')
 
-for row in mentioned_rows:
-    rows = func(row.find_all('td'))
-    if(len(rows) == 5):
-        data.append(rows)
-    else:
-        data.append(rows[1:])
+        mentioned_table = table_content[-1]
+        mentioned_rows = mentioned_table.find_all('tr')
+        mentioned_rows = mentioned_rows[1:-1]
 
-cur_data = {x[0]: {'latest':x[1:]} for x in data}
+        data = []
 
-flag_change = False
-info = []
+        for row in mentioned_rows:
+            rows = func(row.find_all('td'))
+            if(len(rows) == 5):
+                data.append(rows)
+            else:
+                data.append(rows[1:])
 
-prev_data = load()
+        cur_data = {x[0]: {'latest':x[1:]} for x in data}
 
-for state in cur_data:
-    if state not in prev_data:
-        info.append(f'{state} got its first case of corona virus: {cur_data[state]["latest"]}')
-        flag_change = True
-    else:
-        curr = cur_data[state]['latest']
-        prev = prev_data[state]['latest']
-        if prev != curr:
-            info.append(f'Change in the number of cases for {state}: {prev} -> {curr}')
-            flag_change = True
+        flag_change = False
+        info = []
 
-if flag_change:
-    for state in cur_data:
-        prev_data = cur_data
-    save(prev_data)
+        prev_data = load()
 
-table = tabulate(data,headers=header,tablefmt='psql')
-print(table)
-print(info)
+        for state in cur_data:
+            if state not in prev_data:
+                info.append(f'{state} got its first case of corona virus: {cur_data[state]["latest"]}')
+                flag_change = True
+            else:
+                curr = cur_data[state]['latest']
+                prev = prev_data[state]['latest']
+                if prev != curr:
+                    info.append(f'Change in the number of cases for {state}: {prev} -> {curr}')
+                    flag_change = True
+
+        if flag_change:
+            for state in cur_data:
+                prev_data = cur_data
+            save(prev_data)
+
+        table = tabulate(data,headers=header,tablefmt='psql')
+        print(table)
+
+        event_info = ''
+        for event in info:
+            logging.warning(event)
+            event_info += '\n - ' + event.replace("'","")
+
+        print(event_info)
+    except Exception as e:
+        logging.exception('script got failed.')
